86e477735d4e04a1fe8ebb0efb17b8fdca5f773a
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-16 00:18:21 +0200

    Create a base for the homepage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './components/homepage'; 
+
 function App() {
   return (
     <div className="App">
       I am App
+      <HomePage></HomePage>
     </div>
   );
 }
