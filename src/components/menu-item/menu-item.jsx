24d5f7785117cb3ebb20227edb1339d5d91f2438
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-17 02:00:48 +0200

    Refactoring the code of the MenuItem

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import './menu-item.scss';
 
 
-const MenuItem = (props) => (
+const MenuItem = ({ title }) => (
   <div className="menu-item">
     <div className="content">
-      <h1 className="title">{ props.title }</h1>
+      <h1 className="title">{ title }</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
-)
+);
 
 export default MenuItem;
