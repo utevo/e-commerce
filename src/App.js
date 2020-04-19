9279f9712ec4156c085475ad723a32e4ce9aacbf
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-19 15:45:36 +0200

    Create fundations for routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
 
+const HoodiesPage = () => (
+  <div>
+    <h1>HOODIES PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      I am App
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hoodies' component={HoodiesPage} />
+      </Switch>
     </div>
   );
 }
