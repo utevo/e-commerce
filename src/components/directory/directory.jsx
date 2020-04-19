42e44efe077c7eb843f2723c5161b03812d6f086
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-19 15:32:41 +0200

    Add the linkUrl field to section objcect

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -15,28 +15,33 @@ class Directory extends React.Component {
           title: 'hoodies',
           imageUrl: 'https://i.ibb.co/ZdsvBLK/hoodies.jpg',
           id: 1,
+          linkUrl: 'hoodies',
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/Xzxygsw/sneakers.jpg',
           id: 2,
+          linkUrl: '',
         },
         {
           title: 'accessories',
           imageUrl: 'https://i.ibb.co/KGqrWKN/accessories.jpg',
           id: 3,
+          linkUrl: '',
         },
         {
           title: 'men',
           imageUrl: 'https://i.ibb.co/cyVQWth/men.jpg',
           size: 'large',
           id: 4,
+          linkUrl: '',
         },
         {
           title: 'women',
           imageUrl: 'https://i.ibb.co/kJfQ3jP/women.jpg',
           size: 'large',
           id: 5,
+          linkUrl: '',
         },
       ]
     }
@@ -46,8 +51,8 @@ class Directory extends React.Component {
     return (
       <div className='directory'>
         {
-          this.state.sections.map(({title, imageUrl, size, id}) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+          this.state.sections.map(({id, ...otherSectionProps}) => (
+            <MenuItem key={id} {...otherSectionProps} />
           ))
         }
       </div>
