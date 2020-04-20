39e0738039beb3fd3b6ad4ffcc0e5bd9a6ea325d
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-21 01:19:04 +0200

    Create CollectionPreview component

--- src/pages/shoppage/shoppage.jsx
+++ src/pages/shoppage/shoppage.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 
+import CollectionPreview from '../../components/collection-preview/collection-preview';
+
 import SHOP_DATA from './data.js';
+
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      COLLECTION: SHOP_DATA
+      collections: SHOP_DATA
     }
   }
 
   render() {
     return (
-      <div className='shoppage'>SHOPPAGE</div>
+      <div className='shoppage'>
+        {
+          this.state.collections.map((collection) => (
+            <CollectionPreview
+              title={collection.title}
+              items={collection.items} />
+          ))
+        }
+      </div>
     )
   }
 }
