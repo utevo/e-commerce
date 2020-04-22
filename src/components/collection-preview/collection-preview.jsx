8987e28914066c8a15815f40d0d00a67079c730d
Michał Kowieski <kowieski.m98@gmail.com>
2020-04-23 00:39:34 +0200

    Create foundations for the CollectionItem component

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CollectionItem from '../collection-item/collection-item';
+
 import './collection-preview.scss';
 
 
@@ -8,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
       {
-        items.filter((item, i) => i < 4).map((item) => (
-          <div key={item.id}>{item.name}</div>
+        items.filter((item, i) => i < 4).map(({id, ...otherItemProps}) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))
       }
     </div>
