021339c8c12df55afbc424e5a93f83ef1fac6419
Michał Kowieski <michael.kowieski@gmail.com>
2020-05-17 20:20:48 +0200

    Create Header container

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/Home/Homepage';
 import Shoppage from './pages/Shop/Shoppage';
+import Header from './components/Header/Header'
 
 
 function App() {
   return (
     <div className="app">
+      <Header/>
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={Shoppage} />
