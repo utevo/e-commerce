515d8a767d51e2b64be3e9730eec050e21047848
Michał Kowieski <michael.kowieski@gmail.com>
2020-05-30 23:11:06 +0200

    Refactoring the SignUp component

--- src/components/SIgnUp/SignUp.jsx
+++ src/components/SIgnUp/SignUp.jsx
@@ -26,8 +26,8 @@ class SignUp extends React.Component {
 
   handleSubmit = async event => {
     event.preventDefault()
-    
-    const {displayName, email, password, confirmPassword} = this.state
+
+    const { displayName, email, password, confirmPassword } = this.state
     if (password !== confirmPassword) {
       alert("passwords don't match")
       return
@@ -48,7 +48,7 @@ class SignUp extends React.Component {
   }
 
   render() {
-    const {displayName, email, password, confirmPassword} = this.state
+    const { displayName, email, password, confirmPassword } = this.state
     return (
       <div className='sign-up'>
         <h2 className='title'>You don't have an accout?</h2>
